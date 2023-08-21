2a6959435fff253a94ac8586f3dc129d6da303f2
paulrouge <paulrouge@outlook.com>
2023-08-21 09:32:45 +0200

    added test conditional to evm blockchecker

--- internal/evm/blockChecker.go
+++ internal/evm/blockChecker.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"math/big"
 	"time"
+
+	"github.com/paulrouge/xcall-event-watcher/internal/config"
 )
 
 func CheckBlocks() {
@@ -12,33 +14,77 @@ func CheckBlocks() {
 	latestBlock, _ := EVMClient.BlockNumber(context.Background())
 	fmt.Println("currentBlock:", latestBlock)
 
+	if config.TestMode {
+		_ = latestBlock
+		num := config.SepoliaTestBlockHeight
+		bigNum := big.NewInt(int64(num))
+
+		testBlock, err := EVMClient.BlockByNumber(context.Background(), bigNum)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		CurBlockChan <- testBlock
+		return
+	}
+
 	for {
 		currentBlock, _ := EVMClient.BlockNumber(context.Background())
 
 		// sleep 200 ms - to prevent blocking the port of the node?
-		time.Sleep(300 * time.Millisecond)
+		time.Sleep(3 * time.Second)
 
 		// check if there is a new block
 		if currentBlock > latestBlock {
 			// get the block 3 blocks before the current block, to prevent missing transactions/events
 			blockToHandle := currentBlock - 3
 
-			fmt.Printf("Handling Block: %v\n", blockToHandle)
-
 			blockBigInt := new(big.Int)
 			blockBigInt.SetUint64(blockToHandle)
-
 			block, err := EVMClient.BlockByNumber(context.Background(), blockBigInt)
 			if err != nil {
 				fmt.Println(err)
 			}
 
-			fmt.Printf("Handling Txs: %v\n", block.Transactions())
-
-			// CurBlockChan <- blockToHandle
+			CurBlockChan <- block
 			latestBlock = currentBlock
 		}
 
 	}
 
+}
+
+// Handles the blocks sent by the CheckBlocks() function. Checks all transactions in the block.
+func HandleBlock() {
+	for {
+		b := <-CurBlockChan
+
+		// for testing. log the block height.
+		fmt.Printf("Block %d\n", b.NumberU64())
+		fmt.Printf("Amount of transactions: %d\n", len(b.Transactions()))
+
+		for _, tx := range b.Transactions() {
+			// to := tx.To()
+			// fmt.Println("to:", to.Hex())
+			// fmt.Println(tx)
+			TransactionChan <- tx
+		}
+	}
+}
+
+// Handles the transactions sent by the HandleBlock() function. Checks if the transaction is a xCall transaction.
+func HandleTransaction() {
+	for {
+		tx := <-TransactionChan
+		// fmt.Println("tx:",tx.Hash(),"tx to:", tx.To().Hex())
+
+		if tx.To() == nil {
+			// fmt.Println("to is nil")
+			continue
+		}
+
+		if tx.To().Hex() == config.SEPOLIA_BMC_ADDRESS {
+			EVMGetEvents(tx.Hash().Hex())
+		}
+	}
 }
